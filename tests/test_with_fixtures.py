bf51fad0eedb8fd134afe78b9449ce8f5eb16936
Daniel Neumann <daniel.neumann@dkrz.de>
2020-04-16 13:04:40 +0200

    reordered examples using fixtures; fixed #31

--- tests/test_with_fixtures.py
+++ tests/test_with_fixtures.py
@@ -18,22 +18,44 @@ def test_write_int_to_file():
     # assert False
 
 
-# ~~~~ get a temporary directory for our data ~~~~
-# Use case: we need a tmp directory to do some things in there. We could
-#           create our own tmp directory, do the things and, in the end,
-#           remove it. Pytest offers a functionality that provides such a
-#           directory. We just provide `tmpdir` to the call of the test
-#           function.
-#           This `tmpdir` is a so-called `fixture`. Fixtures are functions
-#           that are implicitely called when used as argument in a function
-#           call. Fixtures are meant to generate input data for testing
-#           functions.
-#           Details for the `tmpdir` fixture are provided here:
-#    https://docs.pytest.org/en/latest/reference.html#std:fixture-tmpdir
-#           A list of all available fixtures is provided here:
-#             https://docs.pytest.org/en/latest/fixture.html
+# ~~~~ test write_int_to_file with external definition of tmp directory ~~~~
+# We need a tmp directory to do some things in there. We do not want to set
+# the tmp directory manually, but want to get it from `somewhere`. A decorator
+# could be used for this purpose. Pytest provides a functionality called
+# `fixture` that is a bit simpler to use and to set up.
+# 
+# Fixtures are functions that are implicitely called when used as argument in
+# a function header. NOTE: The fixture is not passed as argument value when the
+# function is called but as argument name in the function definition. Fixtures
+# are meant to generate input data, to prepare something or do some more 
+# advanced stuff for testing functions.
+#
+# Define fixture
+@pytest.fixture
+def get_tmpdir():
+    tmpdir = Path('/tmp')
+    return tmpdir
+
+# use the fixture
 @pytest.mark.use_fixtures
-def test_write_int_to_file_with_fixture(tmpdir):
+def test_write_int_to_file_with_own_fixture(get_tmpdir):
+    file_path = get_tmpdir / 'my_file.txt'
+    write_int_to_file(5, file_path)
+    assert file_path.read_text() == '5'
+
+
+# ~~~~ test write_int_to_file using a predefined fixture ~~~~
+# We use the predefined fixture `tmpdir` that provides us a temporary
+# directory. The nice thing is that we do not have to think about the
+# location of the tmp directory (i.e. for different operating systems).
+# Details for the `tmpdir` fixture are provided here:
+#    https://docs.pytest.org/en/latest/reference.html#std:fixture-tmpdir
+# A list of all available fixtures is provided here:
+#    https://docs.pytest.org/en/latest/fixture.html
+@pytest.mark.use_fixtures
+def test_write_int_to_file_with_predefined_fixture(tmpdir):
+    # `tmpdir` is alreay a `Path` object but it caused strange errors.
+    # Therefore, we do the `Path(str(...))`.
     file_path = Path(str(tmpdir)) / 'my_file.txt'
     write_int_to_file(5, file_path)
     assert file_path.read_text() == '5'
@@ -44,8 +66,9 @@ def test_write_int_to_file_with_fixture(tmpdir):
 
 
 # ~~~~ test output stream ~~~~
-#  see for details:
-#   https://docs.pytest.org/en/latest/reference.html#std:fixture-capfd
+# The `capfd` fixture captures the output and error streams. Thus, the command
+# line output of programms can be tested. For details on `capfd` see:
+#    https://docs.pytest.org/en/latest/reference.html#std:fixture-capfd
 @pytest.mark.use_fixtures
 def test_output(capfd):
     print('Hello World!')
@@ -54,7 +77,7 @@ def test_output(capfd):
 
 
 # ~~~~ test sum_dict ... using our own fixture ~~~~
-# Instead of using existing fixtures, we can also make our own ones.
+# Another example with a self-made fixture
 #
 # We want to test the function `sum_dict`. For this purpose we need a
 # dictionary. Hence, we will create a fixture that creates us a dictionary.
